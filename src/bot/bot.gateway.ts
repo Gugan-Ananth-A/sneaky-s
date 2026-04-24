0a6c999f8e28815ad5cd33d447e3bd0b6979549b
Gugan Ananth <gugan.ananth@rootquotient.com>
2026-04-24 23:31:58 +0530

    Bug Fixes

--- src/bot/bot.gateway.ts
+++ src/bot/bot.gateway.ts
@@ -27,7 +27,7 @@ export class BotGateway {
         )
       ) {
         await message.reply(
-          'You are missing to mention the name! Please, redo the entire template again to validate!',
+          'You are missing to mention the name!\n*-# -- Please post the entire template again to verify --*',
         );
       } else if (
         !(
@@ -35,16 +35,22 @@ export class BotGateway {
           (message.content?.includes('Age') ?? false)
         )
       ) {
-        await message.reply('You are missing to mention the age!');
+        await message.reply(
+          'You are missing to mention the age!\n*-# -- Please post the entire template again to verify --*',
+        );
       } else if (ageValidator(message.content)) {
-        await message.reply('Entered Age is not valid!');
+        await message.reply(
+          'Entered Age is not valid!\n*-# -- Please post the entire template again to verify --*',
+        );
       } else if (
         !(
           (message.content?.includes('gender') ?? false) ||
           (message.content?.includes('Gender') ?? false)
         )
       ) {
-        await message.reply('You are missing to mention the gender!');
+        await message.reply(
+          'You are missing to mention the gender!\n*-# -- Please post the entire template again to verify --*',
+        );
       } else if (
         !(
           (message.content?.includes('kinks') ?? false) ||
@@ -53,7 +59,9 @@ export class BotGateway {
           (message.content?.includes('Kink') ?? false)
         )
       ) {
-        await message.reply('You are missing to mention the kinks!');
+        await message.reply(
+          'You are missing to mention the kinks!\n*-# -- Please post the entire template again to verify --*',
+        );
       } else if (
         !(
           (message.content?.includes('limits') ?? false) ||
@@ -62,7 +70,13 @@ export class BotGateway {
           (message.content?.includes('Limit') ?? false)
         )
       ) {
-        await message.reply('You are missing to mention the limits!');
+        await message.reply(
+          'You are missing to mention the limits!\n*-# -- Please post the entire template again to verify --*',
+        );
+      } else if (message.author.avatar === null) {
+        await message.reply(
+          "It seems you don't have a discord profile picture!\n*-# -- Please add a profile picture, and post the entire template again to verify --*",
+        );
       } else if (
         !(
           (message.content?.includes('secret code') ?? false) ||
@@ -75,10 +89,12 @@ export class BotGateway {
           (message.content?.includes('Secretcode') ?? false)
         )
       ) {
-        await message.reply('You are missing to mention the secret code!');
+        await message.reply(
+          'You are missing to mention the secret code!\n*-# -- Please post the entire template again to verify --*',
+        );
       } else if (secretCodeValidator(message.content)) {
         await message.reply(
-          'Your secret code is not valid. Please check the rules again to find the secret code!\n-# *--There are 2 words hidden somewhere in the rules--*',
+          'Your secret code is not valid. Please check the rules again to find the secret code!\n-# *-- There are 2 words hidden somewhere in the rules --*\n*-# -- Please post the entire template again to verify --*',
         );
       } else if (
         !(
@@ -88,7 +104,7 @@ export class BotGateway {
         )
       ) {
         await message.reply(
-          'Have you grabbed Roles? It seems you are missing roles...',
+          'Have you grabbed Roles? It seems you are missing roles... \n*-# -- Grab the roles you missed to grab, and please post the entire template again to verify --*',
         );
       } else if (
         !(
@@ -97,11 +113,20 @@ export class BotGateway {
             .includes('favourite bondage image') ??
             false) ||
           (message.content?.toLocaleLowerCase().includes('favourite bondage') ??
-            false)
+            false) ||
+          (message.content?.toLocaleLowerCase().includes('bondage image') ??
+            false) ||
+          (message.content
+            ?.toLocaleLowerCase()
+            .includes('favorite bondage image') ??
+            false) ||
+          (message.content?.toLocaleLowerCase().includes('favorite bondage') ??
+            false) ||
+          (message.content?.toLocaleLowerCase().includes('fav') ?? false)
         )
       ) {
         await message.reply(
-          'You are missing to add your favorite bondage image!',
+          'You are missing to add your favourite bondage image!\n*-# -- Please post the entire template again to verify --*',
         );
       } else {
         await message.react('✅');
@@ -112,10 +137,10 @@ export class BotGateway {
 }
 function ageValidator(content: string) {
   if (content.length === 0) return true;
-  const ageMatch = content.match(/age\s*:\s*[^0-9]*(\d+)/i);
+  const ageMatch = content.match(/age\s*:?\s*[^0-9]*(\d+)/i);
   if (ageMatch != null) {
     const age = ageMatch[1];
-    if (parseInt(age) > 18 && parseInt(age) < 100) return false;
+    if (parseInt(age) >= 18 && parseInt(age) < 100) return false;
     return true;
   } else {
     return true;
@@ -124,7 +149,7 @@ function ageValidator(content: string) {
 
 function secretCodeValidator(content: string) {
   if (content.length === 0) return true;
-  const codeMatch = content.match(/secret\s*code\s*:\s*[^a-zA-Z]*([^\n\r]+)/i);
+  const codeMatch = content.match(/secret\s*code\s*:?\s*[^a-zA-Z]*([^\n\r]+)/i);
   if (codeMatch != null) {
     const code = codeMatch[1].trim().toLowerCase().split('*')[0];
     return !(code.includes('ball gag') || code.includes('ballgag'));
